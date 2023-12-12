a54a5d4edd878411642c5c95a8e07fb3b1ad8fa9
Emillianaqueenemmy <emilynakabuye@gmail.com>
2023-12-12 10:53:16 +0300

    Js

---
+++ index.js
@@ -0,0 +1,8 @@
+// JavaScript code illustrating key concepts
+ let message = "Hello, Node.js!";
+ console.log(message)
+ 
+ function greet(name) {
+ return `Greetings, ${name}!`;
+}
+console.log(greet("Emily"))
