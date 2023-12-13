16a6c2f079e8a14d4551ce64bf6fdf91555142cd
Emillianaqueenemmy <emilynakabuye@gmail.com>
2023-12-13 09:32:07 +0300

    Daytwo

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // JavaScript code illustrating key concepts
  let message = "Hello, Node.js!";
  console.log(message)
- 
+
  function greet(name) {
  return `Greetings, ${name}!`;
 }
 console.log(greet("Emily"))
+const numbers=[1,2,3,4,5]
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+   }
